19bd1d89e6af2b3fcaa5c55250a38e27dcc71d3e
joaovictor3g <jvdias79797@gmail.com>
2020-08-03 20:15:53 -0300

    (feat): landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './assets/styles/global.css';
+
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Routes />
+      </div>
+    );
 }
 
 export default App;
